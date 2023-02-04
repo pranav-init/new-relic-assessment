9c9c44a04c06b7a1bd9aa58012b213782b82eb43
pranav-init <npranav.2018@gmail.com>
2023-02-04 23:51:16 +0530

    Adds changes to use regex for text preprocessing.

--- content_preprocessor.py
+++ content_preprocessor.py
@@ -1,28 +1,15 @@
 import logging
+import re
 
 
 class ContentPreprocessor:
     logging.basicConfig(
         format='[%(asctime)s] %(levelname)s : {app.%(funcName)s} %(message)s', level=logging.INFO)
 
-    punctuation_marks = '''!()-[]{};:'"“”’\,<>./?@#$%^&*_~'''
-
     @staticmethod
     def preprocess_content(text: str) -> str:
+        text = re.sub(r'[^\w\s]', '', text).lower()
         list_of_words = text.split()
-        for index in range(len(list_of_words)):
-            current_word = list_of_words[index]
-            current_word_without_punctuation: str = ''
-            for character in current_word:
-                if character not in ContentPreprocessor.punctuation_marks:
-                    # Add the character to the final string, only if it
-                    # is not a punctuation mark.
-                    current_word_without_punctuation += character
-
-            # Converting the word into lowercase, to make it case insensitive
-            current_word = current_word_without_punctuation.lower()
-            list_of_words[index] = current_word
-
         logging.info(
             "Successfully removed case, punctuation marks from the words in the current file.")
         return list_of_words
